94e6e0f4c10fb54fd91adbd13d54490106cb837a
Cameron Moon <cameron@moon.net.au>
2013-07-21 21:56:22 +1000

    Added business logic check to prevent member payments being used for expiring products

--- finance/admin.py
+++ finance/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 
 from finance.models import *
 
@@ -14,11 +15,22 @@ class IncomeAdmin(admin.ModelAdmin):
     list_display = ('description', 'date', 'payment_value', 'category')
 admin.site.register(Income, IncomeAdmin)
 
+class MemberPaymentForm(forms.ModelForm):
+    class Meta:
+        model = MemberPayment
+    def __init__(self, *args, **kwargs):
+        super(MemberPaymentForm, self).__init__(*args, **kwargs)
+        self.fields['product'].queryset = Product.objects.exclude(id__in=ExpiringProduct.objects.all())
+class MemberPaymentAdmin(admin.ModelAdmin):
+    form = MemberPaymentForm
+admin.site.register(MemberPayment, MemberPaymentAdmin)
+
+
+
 
 admin.site.register(IncomeCategory)
 admin.site.register(ExpenseCategory)
 admin.site.register(RecurringExpense)
-admin.site.register(MemberPayment)
 admin.site.register(Product)
 admin.site.register(ExpiringProduct)
 admin.site.register(MembershipProduct)
