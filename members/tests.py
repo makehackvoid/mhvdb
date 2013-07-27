8234dc520323d70a2d9a29a96a275c259fea65ee
Brenda Moon <brenda@moon.net.au>
2013-07-27 22:21:28 +1000

    Fix legacy member tests
    Legacy member tests were not working because classes and functions had
    been renamed.

--- members/tests.py
+++ members/tests.py
@@ -26,7 +26,7 @@ def dummy_member(name):
                   )
 
 
-class MembershipExpiryTest(TestCase):
+class LegacyMembershipExpiryTest(TestCase):
     """
     Test cases form embership expiries
     """
@@ -36,8 +36,8 @@ class MembershipExpiryTest(TestCase):
         self.fred.save()
 
         # Membership type structure
-        self.associate = Membership(membership_name="Associate")
-        self.full = Membership(membership_name="Full")
+        self.associate = LegacyMembership(membership_name="Associate")
+        self.full = LegacyMembership(membership_name="Full")
         self.associate.save()
         self.full.save()
 
@@ -57,55 +57,55 @@ class MembershipExpiryTest(TestCase):
 
     def test_auto_duration(self):
         """
-        If a new MemberPayment gets inserted, its duration (if empty) should automatically be
+        If a new LegacyMemberPayment gets inserted, its duration (if empty) should automatically be
         calculated based on the cost that applies at the time
         """
-        payment = MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        payment = LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                                 member=self.fred, payment_value="480", date=date(2011, 8, 1),
                                 continues_membership=False)  # $40/mo * 12 months
         payment.save()
-        payment = self.fred.memberpayment_set.all()[0]
+        payment = self.fred.legacymemberpayment_set.all()[0]
         self.assertEqual(payment.duration(), 12)
-        self.assertEqual(self.fred.expiry_date(), date(2012, 7, 31))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2012, 7, 31))
 
     def test_renewal_before_expiry(self):
         """
         If a member renews before their membership expires, the expiry date should be added to the end
         of their existing membership period
         """
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2011, 8, 1),
                       continues_membership=False).save()  # $40/mo * 3 months
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2011, 10, 1),
                       continues_membership=True).save()   # 3 more months
-        self.assertEqual(self.fred.expiry_date(), date(2012, 1, 30))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2012, 1, 30))
 
     def test_renewal_after_expiry(self):
         """
         If a member renews after their expiry date, but didn't stop their membership, the renewal
         payment should be backdated
         """
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2011, 8, 1),
                       continues_membership=False).save()  # $40/mo * 3 months
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2011, 12, 1),
                       continues_membership=True).save()   # 3 more months, LATE!
-        self.assertEqual(self.fred.expiry_date(), date(2012, 1, 30))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2012, 1, 30))
 
     def test_break_in_membership(self):
         """
         If a member lapses back to casual, then starts again, then their new membership doesn't
         count back to the old one
         """
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2011, 8, 1),
                       continues_membership=False).save()  # $40/mo * 3 months
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2012, 8, 1),
                       continues_membership=False).save()  # 3 months again
-        self.assertEqual(self.fred.expiry_date(), date(2012, 10, 31))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2012, 10, 31))
 
     def test_pro_rata_over_rate_reduction(self):
         """
@@ -115,10 +115,10 @@ class MembershipExpiryTest(TestCase):
 
         # $200 buys 2 months on 1 May, but would buy 4 months on 1 June... so result
         # should be 3 months
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="200", date=date(2011, 5, 1),
                       continues_membership=False).save()
-        self.assertEqual(self.fred.expiry_date(), date(2011, 7, 30))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2011, 7, 30))
 
     def test_no_pro_rate_over_rate_hike(self):
         """
@@ -127,10 +127,10 @@ class MembershipExpiryTest(TestCase):
         """
 
         # $120 buys 6 months on 1 May, and that's how it stays
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="120", date=date(2011, 5, 1),
                       continues_membership=False).save()
-        self.assertEqual(self.fred.expiry_date(), date(2011, 10, 30))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2011, 10, 30))
 
     def test_pro_rata_applies_renewals_equally(self):
         """
@@ -144,34 +144,34 @@ class MembershipExpiryTest(TestCase):
         # so expectation is:
         # $200 on 1 March buys to 1 May
         # $200 on 1 May is $100 to 1 June, then $100 more to 1 August
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="200", date=date(2011, 3, 1),
                       continues_membership=False).save()
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="200", date=date(2011, 4, 20),
                       continues_membership=True).save()
 
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=jane, payment_value="200", date=date(2011, 3, 1),
                       continues_membership=False).save()
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=jane, payment_value="200", date=date(2011, 5, 10),
                       continues_membership=True).save()
 
-        self.assertEqual(self.fred.expiry_date(), jane.expiry_date())
-        self.assertEqual(self.fred.expiry_date(), date(2011, 7, 29))
+        self.assertEqual(self.fred.membership_expiry_date(), jane.membership_expiry_date())
+        self.assertEqual(self.fred.membership_expiry_date(), date(2011, 7, 29))
 
     def test_free_membership(self):
         """
         Free membership should count as if it was a payment
         """
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="200", date=date(2011, 3, 1),
                       continues_membership=False).save()
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="0", date=date(2011, 4, 1), free_months=2,
                       continues_membership=True).save()
-        self.assertEqual(self.fred.expiry_date(), date(2011, 6, 30))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2011, 6, 30))
 
     def test_mid_term_upgrade(self):
         """
@@ -179,16 +179,16 @@ class MembershipExpiryTest(TestCase):
         and have the expiry date change appropriately
         """
         # $240 buys 6 months associate (to Feb 2012)
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="240", date=date(2011, 8, 1),
                       continues_membership=False).save()
 
         # After 1 month, we upgrade. The $200 left is 5 month associate or 4 month full
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="0", date=date(2011, 9, 1),
                       is_existing_upgrade=True).save()
         self.assertEqual(self.fred.member_type(date(2011, 11, 1)), self.full)
-        self.assertEqual(self.fred.expiry_date(), date(2011, 12, 30))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2011, 12, 30))
 
     def test_mid_term_upgrade_with_payment(self):
         """
@@ -196,14 +196,14 @@ class MembershipExpiryTest(TestCase):
         at the same time
         """
         # $240 buys 6 months associate (to Feb 2012)
-        MemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.associate, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="240", date=date(2011, 8, 1),
                       continues_membership=False).save()
 
         # After 1 month, we upgrade. The $200 left is 5 month associate or 4 month full
         # but we also a pay in $100 for 2 more months of full
-        MemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
+        LegacyMemberPayment(membership_type=self.full, payment_type=BANK_PAYMENT,
                       member=self.fred, payment_value="100", date=date(2011, 9, 1),
                       is_existing_upgrade=True).save()
         self.assertEqual(self.fred.member_type(date(2011, 2, 1)), self.full)
-        self.assertEqual(self.fred.expiry_date(), date(2012, 2, 29))
+        self.assertEqual(self.fred.membership_expiry_date(), date(2012, 2, 29))
